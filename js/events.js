466682290273210e5d981311ea58b6243b6731b8
Leonardo Baron <leonardo.1234baron@gmail.com>
2020-06-16 20:18:12 -0400

    second commit

--- js/events.js
+++ js/events.js
@@ -22,3 +22,58 @@ const storeScroll = () => {
 document.addEventListener('scroll', debounce(storeScroll), { passive: true });
 
 storeScroll();
+
+function getTimeRemaining(endtime){
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor( (total/1000) % 60 );
+  const minutes = Math.floor( (total/1000/60) % 60 );
+  const hours = Math.floor( (total/(1000*60*60)) % 24 );
+  const days = Math.floor( total/(1000*60*60*24) );
+
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds
+  };
+}
+
+
+function showTime(){
+  const deadline = 'July 29 2020 23:59:59 GMT-0300';
+  const remaining = getTimeRemaining(deadline);
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = "AM";
+  
+  if(h == 0){
+      h = 12;
+  }
+  
+  if(h > 12){
+      h = h - 12;
+      session = "PM";
+  }
+  
+  remaining.hours = (remaining.hours < 10) ? "0" + remaining.hours : remaining.hours;
+  remaining.minutes = (remaining.minutes < 10) ? "0" + remaining.minutes :remaining.minutes;
+  remaining.seconds = (remaining.seconds < 10) ? "0" + remaining.seconds : remaining.seconds;
+  
+  var time = remaining.days + "d " + remaining.hours + ":" +  remaining.minutes + ":" +  remaining.seconds;
+  if (window.innerWidth >= 768) {
+    document.getElementById("MyClockDisplay-md").innerText = time;
+    document.getElementById("MyClockDisplay-md").textContent = time;
+  } else {
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+  }
+  
+  
+  setTimeout(showTime, 1000);
+  
+}
+
+showTime();
